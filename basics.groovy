d61786c99b60e17897c87595890c07ebbbceda55
Michal Nyga <mnyga94@gmail.com>
2022-01-02 18:07:41 +0100

    Update basics.groovy

--- basics.groovy
+++ basics.groovy
@@ -5,7 +5,7 @@ comments.*/
 
 //Define variables
 println ""
-println "============="
+println "======Define variables======="
 def variable = "zmienna"
 variable = "nowaZmienna"
 println "Variable = $variable"
@@ -13,8 +13,7 @@ println "Variable = $variable"
 
 //Define lists
 println ""
-println "============="
-println "Define list"
+println "======Define list======="
 def list = ["el1","el2",variable]
 //HINT: Not using quotas "" will show variable.
 println list
@@ -22,16 +21,14 @@ println list
 
 //Define map
 println ""
-println "============="
-println "Define map"
+println "======Define map======="
 def map = ["el1":1,"el2":2]
 println map
 
 
 //Define function
 println ""
-println "============="
-println "Define function"
+println "======Define function======="
 def function(argument1="value",argument2="value2",argument3="value3")
 {
 println "Argument1 = $argument1"
@@ -43,8 +40,7 @@ function("OwnValue","OwnValueFor3")
 
 //IF - conditional statement
 println ""
-println "============="
-println "IF - conditional statement"
+println "======IF - conditional statement======="
 def text = "One day someone will take a look at this line. Anyone?"
 if(text.contains("Anyone"))
 {
@@ -53,4 +49,15 @@ println "There is someone!"
 else
 {
 println "Ouch, there is not."
+}
+println ""
+println "======Another IF======="
+def favoriteAnimals= ["cat","dog","rabbit"]
+if(favoriteAnimals.contains("rabbit"))
+{
+println "Someone here like rabbits!"
+}
+else
+{
+println "Ouch, someone does not like rabbits here."
 }
